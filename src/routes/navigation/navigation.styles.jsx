d61973d4a99515bde324fc5ade91af6467d9817b
InderKumhar0 <inderkumhar58@gmail.com>
2023-04-29 12:30:24 +0530

    error correction

--- src/routes/navigation/navigation.styles.jsx
+++ src/routes/navigation/navigation.styles.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-
 export const NavigationContainer = styled.div`
   height: 70px;
   width: 100%;
@@ -16,14 +15,14 @@ export const LogoContainer = styled(Link)`
 `;
 
 export const NavLinks = styled.div`
-width: 50%;
-height: 100%;
-display: flex;
-align-items: center;
-justify-content: flex-end;
-`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
 
 export const NavLink = styled(Link)`
-padding: 10px 15px;
-cursor: pointer;
-`
+  padding: 10px 15px;
+  cursor: pointer;
+`;
